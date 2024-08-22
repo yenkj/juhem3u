aa651b7d6da75e88d3a603d838055445b3a4a68a
yenkj <49618620+yenkj@users.noreply.github.com>
2024-08-22 21:21:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const sources = [
   },
 //  {
 //    name: '四季',
-//    url: 'http://us.199301.xyz:6000/4g.m3u'
+//    url: 'http://us.199301.xyz:6000/4g_proxy.m3u'
 //  },
   {
     name: 'xt4g',
@@ -31,7 +31,7 @@ const sources = [
   },
   {
     name: 'beesport',
-    url: 'http://us.199301.xyz:6000/b.m3u'
+    url: 'http://us.199301.xyz:6000/b_proxy.m3u'
   },
   {
     name: 'thetv',
