54811dccdd1bcab29967a0ee5c5601d70b1fbc4c
yenkj <49618620+yenkj@users.noreply.github.com>
2024-08-29 19:17:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const sources = [
 //    name: 'feng hua',
 //    url: 'http://us.199301.xyz:6000/fh.m3u'
 //  },  
-  {
-     name: '央视频',
-     url: 'http://us.199301.xyz:6000/ysp.m3u'
-  },
 //  {
-//    name: 'itv_hevc',
-//    url: 'http://us.199301.xyz:6000/itv_hevc.m3u'
+//     name: '央视频',
+//     url: 'http://us.199301.xyz:6000/ysp.m3u'
 //  },
+  {
+    name: 'itv_hevc',
+    url: 'http://us.199301.xyz:6000/itv_hevc.m3u'
+  },
 //  {
 //    name: '四季',
 //    url: 'http://us.199301.xyz:6000/4g_proxy.m3u'
