4e5dce81fa3fa223bf638c88d03ce3afc1a31015
yenkj <49618620+yenkj@users.noreply.github.com>
2025-03-08 19:03:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 3000;
 const sources = [
     { name: 'feng hua', url: 'http://us.199301.xyz:6000/fh.m3u' },
     { name: 'yst4', url: 'http://us.199301.xyz:6000/yst4.m3u' },
+//    { name: 'fhrp', url: 'http://us.199301.xyz:6000/fhrp.m3u' },
+//    { name: 'bptv', url: 'http://us.199301.xyz:6000/bptv.m3u' },
+//    { name: 'itv', url: 'http://us.199301.xyz:6000/itv_hevc2.m3u' },
+//    { name: 'xtbestzb', url: 'http://us.199301.xyz:6000/xtbestzb.m3u' },
+//    { name: '四季', url: 'http://us.199301.xyz:6000/4g_proxy.m3u' },
     { name: 'xt4g', url: 'http://us.199301.xyz:6000/xt4g.m3u' },
     { name: 'xthami', url: 'http://us.199301.xyz:6000/xthami.m3u' },
     { name: 'thetv', url: 'http://us.199301.xyz:6000/xtttv.m3u' },
