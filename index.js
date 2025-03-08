01fca459c735f011fcb9add5dc5c2bfb93b53042
yenkj <49618620+yenkj@users.noreply.github.com>
2025-03-08 17:15:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const sources = [
 //    { name: 'itv', url: 'http://us.199301.xyz:6000/itv_hevc2.m3u' },
 //    { name: 'xtbestzb', url: 'http://us.199301.xyz:6000/xtbestzb.m3u' },
     { name: 'yst4', url: 'http://us.199301.xyz:6000/yst4.m3u' },
+//    { name: '四季', url: 'http://us.199301.xyz:6000/4g_proxy.m3u' },
     { name: 'xt4g', url: 'http://us.199301.xyz:6000/xt4g.m3u' },
     { name: 'xthami', url: 'http://us.199301.xyz:6000/xthami.m3u' },
     { name: 'thetv', url: 'http://us.199301.xyz:6000/xtttv.m3u' },
